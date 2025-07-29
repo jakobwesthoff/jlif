2f9e56b91db0e9fd50df165f9e7241453bf8354f
Jakob Westhoff <jakob@westhoffswelt.de>
2025-07-29 21:51:24 +0200

    Wired everything already implemented up for now.
    Of course a lot of features, even the ones defined in our Args, are
    still missing, like formatting and filtering. They will follow shortly

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,24 @@ mod buffer;
 mod cli;
 mod processor;
 
+use anyhow::Result;
+use buffer::LineBuffer;
 use clap::Parser;
 use cli::JlifArgs;
+use processor::StreamProcessor;
+use std::io;
 
-fn main() {
+fn main() -> Result<()> {
     let args = JlifArgs::parse();
 
-    println!("Max lines: {}", args.max_lines);
-    println!("Filter: {:?}", args.filter);
-    println!("Case sensitive: {}", args.case_sensitive);
-    println!("JSON only: {}", args.json_only);
+    // Create LineBuffer with user-specified max_lines
+    let line_buffer = LineBuffer::new(args.max_lines);
+
+    // Create StreamProcessor with stdin and stdout
+    let mut stream_processor = StreamProcessor::new(io::stdin(), io::stdout(), line_buffer);
+
+    // Process the stream
+    stream_processor.process()?;
+
+    Ok(())
 }
